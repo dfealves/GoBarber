b76b93cca66b0540f4513db5a0fb65c08bb7e9ba
dfealves <daniloferreira.alves@outlook.com>
2019-12-24 21:17:29 -0300

    add, auth, store perfil, persist auth, load auth toasts error

--- gobarberweb/src/store/modules/auth/sagas.js
+++ gobarberweb/src/store/modules/auth/sagas.js
@@ -1,27 +1,53 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 import history from '~/services/history';
 import api from '~/services/api';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuário não é um prestador');
+    if (!user.provider) {
+      toast.error('Usuário não é um prestador');
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    toast.error('Falha na autenticação, verifique os seus dados');
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (error) {
+    toast.error('Falha no cadastro, verifique seus dados!');
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
