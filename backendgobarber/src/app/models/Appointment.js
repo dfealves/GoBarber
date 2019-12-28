fc5cfdba2b5f5ccbec3bfe6523c63ec5a477bfab
dfealves <daniloferreira.alves@outlook.com>
2019-12-28 14:25:14 -0300

    fix update avatar, add dashboard schedule, date-fns

--- backendgobarber/src/app/models/Appointment.js
+++ backendgobarber/src/app/models/Appointment.js
@@ -1,38 +1,41 @@
 import Sequelize, { Model } from 'sequelize';
 import { isBefore, subHours } from 'date-fns';
 
-class Appointments extends Model {
+class Appointment extends Model {
   static init(sequelize) {
     super.init(
       {
         date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
         past: {
+          // Propriedade virtual não persiste na tabela
           type: Sequelize.VIRTUAL,
           get() {
+            // retorna true caso horário já tenha passado
             return isBefore(this.date, new Date());
           },
         },
         cancelable: {
           type: Sequelize.VIRTUAL,
+          // verifica se o horário atual é no mínimo 2 horas antes do agendamento
           get() {
             return isBefore(new Date(), subHours(this.date, 2));
           },
         },
       },
-      {
-        sequelize,
-      }
+      { sequelize }
     );
 
     return this;
   }
 
+  // Faz o relacionamento do campo user_id com a tabela User
+  // N(agendamentos):1(usuario)
+  // N(agendamentos):1(prestador de servico)
   static associate(models) {
-    // quando uma model (tabela) tem relacionamento duas vezes com outro model(tabela), somos obrigado a determinar um apelido para o relacionamento as: 'user', as: 'provider'
     this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
     this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
   }
 }
 
-export default Appointments;
+export default Appointment;
